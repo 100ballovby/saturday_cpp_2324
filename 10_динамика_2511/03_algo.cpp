b2b4d502da851afdadc6b6f56895b3ac8230575c
greatraksin <greatraksin@icloud.com>
2023-12-02 17:58:21 +0300

    Adding matrix

---
+++ 10_динамика_2511/03_algo.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <random>
+using namespace std;  // подключаю ВСЕ функции из пространства std в свой проект
+
+int rand_int(int min, int max) {
+    random_device rd;  // сам генератор
+    mt19937 gen(rd());  // занимается генерацией числа
+    uniform_int_distribution<int> distribution(min, max);
+    return distribution(gen);
+}
+
+void print(int *array, int size) {  // передача массива по указателю в функцию
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << "\t";
+    }
+}
+
+// передача массива по указателю в функцию
+void fill_array(int *arr, int length) {
+    for (int i = 0; i < length; i++) {
+        arr[i] = rand_int(1, 100);
+    }
+}
+
+int binary_search(int *array, int size, int key) {
+    int low { 0 };
+    int high { size - 1 };
+    int mid = (low + high) / 2;
+
+    while (low <= high) {  // пока не нашли искомое в массиве И пока массив не схлопнулся
+        if (array[mid] == key) {
+            return mid;
+        } else if (array[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+        mid = (low + high) / 2;  // переменная mid должна пересчитываться с каждым повторением цикла ВНЕ зависимости от того low или high была заменена
+    }
+    return -1;  // если число в массиве не было найдено
+}
+
+void bubble_sort(int *array, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                int tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
+}
+
+int algo_main_2511() {
+    int array[100];
+    fill_array(array, 100);  // наполним массив случайными числами в количестве 100 штук
+    print(array, 100);
+    cout << endl;
+    bubble_sort(array, 100);   // отсортируем массив
+    print(array, 100);
+
+    cout << endl;
+    int key, res;
+    key = 95;
+    res = binary_search(array, 100, key);
+    cout << "Index of " << key << " in array is: " << res;
+    return 0;
+}
+
+
--- main.cpp
+++ main.cpp
@@ -1,71 +1,86 @@
 #include <iostream>
 #include <string>
-#include <random>
 using namespace std;  // подключаю ВСЕ функции из пространства std в свой проект
 
-int rand_int(int min, int max) {
-    random_device rd;  // сам генератор
-    mt19937 gen(rd());  // занимается генерацией числа
-    uniform_int_distribution<int> distribution(min, max);
-    return distribution(gen);
-}
 
-void print(int *array, int size) {  // передача массива по указателю в функцию
-    for (int i = 0; i < size; i++) {
-        cout << array[i] << "\t";
-    }
-}
-
-// передача массива по указателю в функцию
-void fill_array(int *arr, int length) {
-    for (int i = 0; i < length; i++) {
-        arr[i] = rand_int(1, 100);
-    }
-}
-
-int binary_search(int *array, int size, int key) {
-    int low { 0 };
-    int high { size - 1 };
-    int mid = (low + high) / 2;
-
-    while (low <= high) {  // пока не нашли искомое в массиве И пока массив не схлопнулся
-        if (array[mid] == key) {
-            return mid;
-        } else if (array[mid] < key) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
+void add_matrix() {
+    int A[3][3] = {
+            {4, 5,  2},
+            {6, -8, 1},
+            {9, 7,  3}
+    };
+    int B[3][3] = {
+            {9,  1, -6},
+            {-3, 5, -12},
+            {-6, 3, 7}
+    };
+    for (int i = 0; i < 3; i++) {  // цикл совершает обход по строкам
+        for (int j = 0; j < 3; j++) {  // цикл совершает обход по столбцам
+            cout << A[i][j] + B[i][j] << "\t";
         }
-        mid = (low + high) / 2;  // переменная mid должна пересчитываться с каждым повторением цикла ВНЕ зависимости от того low или high была заменена
+        cout << endl;
     }
-    return -1;  // если число в массиве не было найдено
 }
 
-void bubble_sort(int *array, int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                int tmp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = tmp;
+void mult_matrix() {
+    int A[3][3] = {
+            {4, 5, 2},
+            {6, -8, 1},
+            {9, 7, 3}
+    };
+    int B[3][1] = {
+            {9},
+            {-3},
+            {-6}
+    };
+    int C[3][1];
+    cout << "Multiplying matrix" << endl;
+    for (int i = 0; i < 3; i++) {  // цикл совершает обход по строкам
+        for (int j = 0; j < 1; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < 3; k++) {
+                C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 1; j++) {
+            cout << C[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
 }
 
-int main() {
-    int array[100];
-    fill_array(array, 100);  // наполним массив случайными числами в количестве 100 штук
-    print(array, 100);
-    cout << endl;
-    bubble_sort(array, 100);   // отсортируем массив
-    print(array, 100);
+void transpose_matrix() {
+    int A[3][3] = {
+            {4, 5, 2},
+            {6, -8, 1},
+            {9, 7, 3}
+    };
+    cout << "Transponded matrix:" << endl;
+    int transponded[3][3];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            transponded[i][j] = A[j][i];
+            cout << transponded[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
 
-    cout << endl;
-    int key, res;
-    key = 95;
-    res = binary_search(array, 100, key);
-    cout << "Index of " << key << " in array is: " << res;
+
+int main() {
+    int A[3][4] = {
+            {4, 5, 2, -8},
+            {6, -8, 1, -6},
+            {9, 7, 3, -7}
+    };
+    cout << "main axis" << endl;
+    for (int i = 0; i < 3; i++) {
+        cout << A[i][3 - 1 - i] << " ";
+    }
     return 0;
 }
 
