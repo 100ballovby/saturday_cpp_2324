0b523572c6b7dbf459b38c0e47cc862a11a34cec
GreatRaksin <greatraksin@icloud.com>
2024-03-23 18:00:55 +0300

    Adding algo big-O

---
+++ 23_algorithms_1603/sorting.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iterator>  // добавить заголовочный файл iterator
+using namespace std;
+
+void sortPointers(vector<int> &my_v) {
+    int n = my_v.size();
+    for (int j = 1; j < n; j++) {  // цикл проходит по элементам массива со 2 элемента до последнего (первый считается отсортированным в первом проходе)
+        int i = j - 1;  // фиксируем элемент, предшествующий текущему элементу j
+        int value = my_v[j];  // фиксируем значение элемента массива, на который указывает j
+        while (i >= 0 && my_v[i] > value) {  // цикл-заполнитель
+            my_v[i + 1] = my_v[i];
+            i--;
+        }
+        my_v[i + 1] = value;
+    }
+}
+
+void selectionSort(vector<int> &my_v) {
+    int n = my_v.size();
+    for (int i = 0; i < n - 1; i++) {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++) {
+            if (my_v[j] < my_v[min_index]) {
+                min_index = j;
+            }
+        }
+        if (min_index != i) {
+            swap(my_v[i], my_v[min_index]);
+        }
+    }
+}
+
+int sorting_main() {
+    vector<int> array { 5, 2, 4, 1, 9, 6, 0 };
+    //sortPointers(array);
+    selectionSort(array);
+    // вывести отсортированный массив
+    for (int num : array) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
